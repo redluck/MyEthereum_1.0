30524510a8e5c14b479f07f3ae5d12f9b31930e1
redluck <redluck@tiscali.it>
2017-09-26 17:10:59 +0200

    Cancellazione commento

--- app/src/main/java/it/redluck/materialdesign/activity/MainActivity.java
+++ app/src/main/java/it/redluck/materialdesign/activity/MainActivity.java
@@ -81,7 +81,7 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
                 fragment = new Fragment02();
                 tag = "frag02";
                 break;
-            /*case 2: //PROVA
+            /*case 2:
                 fragment = new Fragment03();
                 tag = "frag03";
                 break;*/
